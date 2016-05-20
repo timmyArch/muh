924c8c074fad296d46faf2fc611d8f3c083fa62a
Flipez <github@flipez.net>
2016-05-20 16:56:58 +0000

    add possibility to create gists
    Signed-off-by: Flipez <github@flipez.net>

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -1,25 +1,24 @@
 class GistsController < ApplicationController
 
-  def index
-  end
-
-  def new
-  end
-
-  def create
-    a = Gist.new
-    params[:snippets].each do |x|
-      a.snippets.create Snippet.new(x)
-    end
-    redirect_to gists_url(id: a.id)
-  end
-
   before_action :get_gist, only: [:show, :delete]
 
   def new
     @languages = CODEMIRROR[:modes]
   end
   def show; end
+
+  def create
+
+    snippets = params['gist']
+    langs = params['lang']
+
+    gist = Gist.new
+    snippets.each_with_index do |s,i|
+      gist.snippets.create(Snippet.new(paste: s, lang: langs[i]))
+    end
+    flash[:created] = {title: 'Successfully created gist.', id: gist.id}
+    redirect_to "/gists/#{gist.id}"
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  
+
+  post 'gists' => 'gists#create'
+
   get 'gists/new' => 'gists#new'
   get 'gists/:id' => 'gists#show', as: :gist
 end
